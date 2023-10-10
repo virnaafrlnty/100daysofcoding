b90813b1289b95d323a737b13b102890899dc965
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-10-10 22:40:58 +0800

    Day 2 -> belajar variabel dan tipe data

---
+++ day2.java
@@ -0,0 +1,19 @@
+
+package pkg100dayss;
+ 
+public class day2 {
+    public static void main(String[] args) {
+        //deklarasi dan inisialisasi varibel
+        String nama = "Virna";
+        int umur = 20;
+        char jenisKelamin = 'P';
+        boolean sudahMenikah = false;
+        
+        
+        System.out.println("Nama    :" + nama);
+        System.out.println("Umur    :" + umur);
+        System.out.println("Jenis Kelamin    :" + jenisKelamin);
+        System.out.println("Sudah Menikah    :" + sudahMenikah);
+    }
+    
+}
