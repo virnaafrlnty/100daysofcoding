46e5dfd7dac6f940ca59dfb40df4c2d59450e887
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-10-11 23:38:43 +0800

    Add files via upload
    Day 3 -> belajar operasi aritmatika

---
+++ day3.java
@@ -0,0 +1,26 @@
+package pkg100dayss;
+
+public class day3 {
+     public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 5;
+        
+        //penjumlahan
+        int HasilTambah = angka1 + angka2;
+        
+        //pegurangan
+        int HasilKurang = angka1 - angka2;
+        
+        //perkalian
+        int HasilKali = angka1 * angka2;
+        
+        //pembagian
+        int HasilBagi = angka1/angka2;
+        
+        
+        System.out.println("Hasil penjmlahan    :" + HasilTambah);
+        System.out.println("Hasil pengurangan   :" + HasilKurang);
+        System.out.println("Hasil perkalian     :" + HasilKali);
+        System.out.println("Hasil pembagian     :" + HasilBagi);
+     }
+}
