dda233f7a30172999ec66ae5605a20958c4249db
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-10-12 22:40:51 +0800

    Add files via upload
    Day 4 -> belajar operasi aritmatika lagi (lanjutan)

---
+++ day4.java
@@ -0,0 +1,22 @@
+package pkg100dayss;
+
+public class day4 {
+    public static void main(String[] args) {
+        int angka1 = 5;
+        int angka2 = 2;
+        double hasilPembagianDesimal = (double) angka1/angka2;
+        int sisaPembagian = angka1 % angka2;
+        
+        angka1++;
+        angka2--;
+        
+        System.out.println("hasil pembagian (dengan desimal)    :" + hasilPembagianDesimal);
+        System.out.println("sisa pembagian  :" +sisaPembagian);
+        System.out.println("hasil increment (penambahan satu angka)    :" + angka1);
+        System.out.println("hasil decrement (pengurangan satu angka)    :" + angka2);
+        
+        
+        
+    }
+    
+}
