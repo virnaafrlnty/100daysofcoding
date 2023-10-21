f7b832839d35eb37c78d6e3842d828d64f5745e6
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-10-21 22:01:42 +0800

    array menggunakan perulangan for-each

---
+++ day13.java
@@ -0,0 +1,13 @@
+package pkg100dayss;
+
+public class day13 {
+    public static void main(String[] args) {
+        // Mendefinisikan dan menginisialisasi array of strings
+        String[] namaHari = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu", "baru wkwkw"};
+
+        // Mengakses dan mencetak elemen-elemen dari array of strings
+        for (String hari : namaHari) {
+            System.out.println("Hari ini adalah " + hari);
+        }
+    }
+}
