fe316b0b269dac3abc8720ef8524f48b65c5fc3a
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-02 23:25:23 +0800

    belajar array 1D

---
+++ day25.java
@@ -0,0 +1,16 @@
+package pkg100dayss;
+
+public class day25 {
+    public static void main(String[] args) {
+        // Array 1D dengan tipe data int
+        int[] intArray = {1, 2, 3, 4, 5};
+       
+
+        // Mencetak elemen-elemen array
+        System.out.println("intArray:");
+        for (int num : intArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
+
