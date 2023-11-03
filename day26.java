8763f7f8f8fbe4bc8c8f3af7ef4e60ef8bdf08bc
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-03 23:43:18 +0800

    masih belajar array 1D

---
+++ day26.java
@@ -0,0 +1,15 @@
+package pkg100dayss;
+
+public class day26 {
+    public static void main(String[] args) {
+        // Mendeklarasikan dan menginisialisasi array 1D
+        int[] angka = {10, 20, 30, 40, 50};
+
+        // Menampilkan elemen-elemen array
+        System.out.println("Isi dari array angka:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Angka ke-" + i + ": " + angka[i]);
+        }
+    }
+}
+
