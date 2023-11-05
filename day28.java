15cf7ff18eccb59886a5e4ca8964622a5f95df3a
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-05 19:12:55 +0800

    masih belajar array 1D

---
+++ day28.java
@@ -0,0 +1,16 @@
+package pkg100dayss;
+
+public class day28 {
+    public static void main(String[] args) {
+        // Array 1D dengan tipe data String
+        String[] intArray = {"virna", "irma", "nurul", "randa", "fardan", "dhani"};
+       
+
+        // Mencetak elemen-elemen array
+        System.out.println("Nama:");
+        for (String num : intArray) {
+            System.out.println(num + " ");
+        }
+    }
+}
+
