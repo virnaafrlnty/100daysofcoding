6800ff5b88b59565f52ebfd13e6b2dff2000ca46
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-06 05:51:49 +0800

    masih belajar array 1D

---
+++ day29.java
@@ -0,0 +1,14 @@
+package pkg100dayss;
+
+public class day29 {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5}; // Array 1D dengan tipe data int
+       
+
+        System.out.println("intArray:");  // Mencetak elemen-elemen array
+        for (int num : intArray) {
+            System.out.println("angka ke-" + num + " adalah index ke- ");
+        }
+    }
+}
+
