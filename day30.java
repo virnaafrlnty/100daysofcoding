7899a397a223ce252a3eafad44e3db93aae8061d
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-07 11:30:24 +0800

    belajar array 1D lagi

---
+++ day30.java
@@ -0,0 +1,12 @@
+package pkg100dayss;
+
+public class day30 {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5}; // Mendefinisikan dan menginisialisasi array 
+
+        
+        for (int nomor : angka) { // Mengakses dan mencetak elemen-elemen dari array
+            System.out.println("Ini adalah nomor " + nomor);
+        }
+    }
+}
