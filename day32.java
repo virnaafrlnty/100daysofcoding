cd2c92569ec8ef7794d62c93b1d8cb435d1fe4ab
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-09 21:50:36 +0800

    belajar variabel, tipe data, dan percabangan

---
+++ day32.java
@@ -0,0 +1,34 @@
+package pkg100dayss;
+
+public class day32 {
+    public static void main(String[] args) {
+        String nama_depan = "Virna";
+        String nama_belakang = "Afrilianty";
+        int umur = 20;
+        double tinggi_badan = 150;
+        double berat_badan = 40;
+        char size_baju = 'L';
+        boolean status = false;
+        
+        System.out.println("---------------DATA DIRI---------------");
+        System.out.println("Nama lengkap        :" + nama_depan + nama_belakang);
+        System.out.println("Umur                :" + umur + "tahun");
+        System.out.println("Tinggi Badan        :" + tinggi_badan + "cm");
+        System.out.println("Berat Badan         :" + berat_badan + "kg");
+        System.out.println("Ukuran Baju         :" + size_baju);
+        
+        
+         if (status == true){
+            System.out.println(" Status Pernikahan  :Menikah");
+        } else{
+            System.out.println("Status Pernikahan   :Belum Menikah");
+        }
+        
+        System.out.println("---------------------------------------");
+        
+    }
+    
+}
+
+    
+
