32f50832737905422142dc6d4e815160cd6aaf4e
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-10 23:38:08 +0800

    belajar inputan, operator, dan percabaangan

--- day32.java
+++ day32.java
@@ -1,34 +1,30 @@
 package pkg100dayss;
+import java.util.Scanner;
+
 
 public class day32 {
     public static void main(String[] args) {
-        String nama_depan = "Virna";
-        String nama_belakang = "Afrilianty";
-        int umur = 20;
-        double tinggi_badan = 150;
-        double berat_badan = 40;
-        char size_baju = 'L';
-        boolean status = false;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan nilai x:");
+        int x = scan.nextInt();
         
-        System.out.println("---------------DATA DIRI---------------");
-        System.out.println("Nama lengkap        :" + nama_depan + nama_belakang);
-        System.out.println("Umur                :" + umur + "tahun");
-        System.out.println("Tinggi Badan        :" + tinggi_badan + "cm");
-        System.out.println("Berat Badan         :" + berat_badan + "kg");
-        System.out.println("Ukuran Baju         :" + size_baju);
+        System.out.print("Masukkan nilai y:");
+        int y = scan.nextInt();
         
+        int q;
         
-         if (status == true){
-            System.out.println(" Status Pernikahan  :Menikah");
-        } else{
-            System.out.println("Status Pernikahan   :Belum Menikah");
-        }
-        
-        System.out.println("---------------------------------------");
+        int p = x + y;
+            if (p >= 4){
+                q = x*y;
+            }else{
+                q = x/y;
+            }
+            System.out.println("hasil:" + q);
         
     }
     
 }
+
 
     
 
