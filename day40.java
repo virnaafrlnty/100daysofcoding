88cfe631015ed4c2b9d8021d252f6168978c821b
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-16 22:26:08 +0800

    belajar percabangan dalam perulangan

---
+++ day40.java
@@ -0,0 +1,15 @@
+package pkg100dayss;
+
+public class day40 {
+    public static void main(String[] args) {
+        int angka = 10;
+        
+        for (int i = 1; i < 10; i++) {
+            if (i %2 == 0) {
+                System.out.println(i);
+                
+            }   
+        }
+    }
+    
+}
