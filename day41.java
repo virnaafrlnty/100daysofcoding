02191080341c48b1a49adb391e0775deeeb0a6d4
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-17 16:44:15 +0800

    belajar perulangan

---
+++ day41.java
@@ -0,0 +1,13 @@
+package pkg100dayss;
+
+public class day41 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("angka ke- " + i);
+             total += i;
+        }
+        System.out.println("total: " + total);
+    }
+    
+}
