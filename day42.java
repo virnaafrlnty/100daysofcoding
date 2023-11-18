6309f4cb42de8c379dd6b27519d0163e3b2f72f1
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-18 23:58:36 +0800

    belajar perulangan while

---
+++ day42.java
@@ -0,0 +1,19 @@
+package pkg100dayss;
+
+public class day42 {
+
+    public static void main(String[] args) {
+        int total = 0;
+        int i = 2;
+        
+        while (i <= 20) {
+            System.out.println(i);
+            total += i;
+            i += 2;
+
+        }
+        System.out.println("totalnya adalah: " + total);
+    }
+
+}
+
