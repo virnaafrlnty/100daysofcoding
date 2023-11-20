81a871eb214d121aecda6108c5689e25fac743dd
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-20 21:10:01 +0800

    menampilkan bilangan ganjil 1-100 menggunakan perulangan

---
+++ day44.java
@@ -0,0 +1,10 @@
+package pkg100dayss;
+
+public class day44 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
