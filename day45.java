e0f61f02aa32257ba43a6a1a3304716b738266c7
virnaafrlnty <virnaafrilianty23@gmail.com>
2023-11-21 20:22:18 +0800

    menentukan bilangan ganjil dan genap menggunakan percabangan dalam perulangan

---
+++ day45.java
@@ -0,0 +1,14 @@
+package pkg100dayss;
+
+public class day45 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " adalah bilangan genap.");
+            } else {
+                System.out.println(i + " adalah bilangan ganjil.");
+            }
+        }
+    }
+}
