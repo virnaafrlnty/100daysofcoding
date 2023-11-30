7b60ed12ecada2ec4dde47974da7e4ddf798771e
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-11-30 06:47:14 -0800

    belajar looping dalam looping.

---
+++ day54.java
@@ -0,0 +1,13 @@
+package pkg100dayss;
+
+public class day54 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
