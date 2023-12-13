d598e6195d1d5e5d839006f3ed31795d2671f35b
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-12-13 08:00:05 -0800

    Pesan hari ini: Izin Sakit

---
+++ day67.java
@@ -0,0 +1,14 @@
+package pkg100dayss;
+
+import java.util.Scanner;
+
+public class day67 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan pesanmu: ");
+        String pesan = input.nextLine();
+        System.out.println("Pesan hari ini: " + pesan);
+        
+    }
+}
