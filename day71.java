443325a27584def6a7a5f5a06945d6411c6e5261
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-12-17 04:16:58 -0800

    belajar array lagi

---
+++ day71.java
@@ -0,0 +1,17 @@
+package pkg100dayss;
+
+public class day71 {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+        
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Elemen ke-" + i + a[i]);          
+        }
+    }   
+}
