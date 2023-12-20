2d5300ac59564254e5a62d6e85b9beb50718c957
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-12-20 06:39:22 -0800

    update nilai sebuah variabel

---
+++ day74.java
@@ -0,0 +1,16 @@
+package pkg100dayss;
+
+public class day74 {
+    public static void main(String[] args) {
+        String namaDepan =  "ahmad";
+        String namaBelakang = "junaedi";
+        
+        namaDepan= "Muhammad";
+        namaBelakang = "Hamdani";
+        
+        System.out.println("Nama Depan: " + namaDepan);
+        System.out.println("Nama Belakang: " + namaBelakang);
+    }   
+}
+
+    
