4ec4d0b5ee4e61ee506cb0fb9bfb2f8985a5c969
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2023-12-22 07:01:30 -0800

    hitung lama menabung

---
+++ day76.java
@@ -0,0 +1,11 @@
+package pkg100dayss;
+
+public class day76 {
+    public static void main(String[] args) {
+        int hargaLaptop = 55_000_000;
+        int tabunganPerBulan = 1_500_000;
+        
+        int lamaMenabung = hargaLaptop/tabunganPerBulan;
+        System.out.println("Lama menabung = " + lamaMenabung + " bulan");        
+    }    
+}
