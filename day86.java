11fc2d86967197cf5830bec712ce658fbcfb183d
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2024-01-01 06:30:45 -0800

    mencetak angka acak

---
+++ day86.java
@@ -0,0 +1,14 @@
+package pkg100dayss;
+
+import java.util.Random;
+
+public class day86 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int number = random.nextInt(101);
+
+        System.out.println("Angka acak: " + number);
+    }
+}
+
