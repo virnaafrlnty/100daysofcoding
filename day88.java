2e99996b1177a987d6be1d3aba592cd7f8f8d50d
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2024-01-03 06:59:21 -0800

    perulangan do while

---
+++ day88.java
@@ -0,0 +1,11 @@
+package pkg100dayss;
+
+public class day88 {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+    }
+}
