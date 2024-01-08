17b5f822987bff2e20bbf7b4e1fbb45ba956e400
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2024-01-08 05:22:05 -0800

    belajar function

---
+++ day93.java
@@ -0,0 +1,10 @@
+package pkg100dayss;
+
+public class day93 {
+    public static void CetakNama(String nama) {
+        System.out.println("Nama Saya adalah: " + nama);
+    }
+    public static void main(String[] args) {
+        CetakNama("Virna");
+    }
+}
