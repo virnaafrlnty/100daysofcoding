633b249b2d16e39655eadbfb082664eefa28dd11
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2024-01-13 04:39:53 -0800

    hitung luas keliling persegi

---
+++ day98.java
@@ -0,0 +1,13 @@
+package pkg100dayss;
+
+public class day98 {
+
+    public static void main(String[] args) {
+        int sisi = 10;
+        double luas = sisi * sisi;
+        int keliling = 4 * sisi;
+        System.out.println("Luas persegi dengan sisi " + sisi + " adalah " + luas);
+        System.out.println("Keliling persegi dengan sisi " + sisi + " adalah " + keliling);
+    }
+}
+
