f7aa868af1f88e2a8730c971ed2c9aef373557ae
Ya, Virna <99790887+virnaafrlnty@users.noreply.github.com>
2024-01-14 06:48:49 -0800

    menghitung luas segitiga

---
+++ day99.java
@@ -0,0 +1,10 @@
+package pkg100dayss;
+
+public class day99 {
+    public static void main(String[] args) {
+        float alas = 10;
+        float tinggi = 5;
+        float luas = (alas * tinggi) / 2;
+        System.out.println("Luas segitiga dengan alas " + alas + " dan tinggi " + tinggi + " adalah " + luas);
+    }
+}
